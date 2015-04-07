6d4e8147a69493fe204a641a0884b06585316332
Daniel Du <duchangyu@gmail.com>
2015-04-07 16:16:04 +0800

    wizard for Acad2016

--- ObjectARXWizardsInstaller/make.bat
+++ ObjectARXWizardsInstaller/make.bat
@@ -8,7 +8,7 @@ subst w: "C:\Program Files (x86)\WiX Toolset v3.8"
 w:\bin\candle.exe ObjectARXWizards.wxs -out temp\ObjectARXWizards.wixobj
 w:\bin\light.exe -sw1076 -b .. temp\ObjectARXWizards.wixobj -out ObjectARXWizards.msi 
 rem -ext WixUIExtension
-if exist ObjectARXWizards-2015.zip del ObjectARXWizards-2015.zip > nul
-"C:\Program Files\WinRAR\WinRAR.exe" a ObjectARXWizards-2015.zip ObjectARXWizards.msi > nul
+if exist ObjectARXWizards-2016.zip del ObjectARXWizards-2016.zip > nul
+"C:\Program Files\WinRAR\WinRAR.exe" a ObjectARXWizards-2016.zip ObjectARXWizards.msi > nul
 
 pause
